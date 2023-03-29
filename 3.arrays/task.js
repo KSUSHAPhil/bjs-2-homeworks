69cb1350c467852c0b7c0de8f9cac277219b7b40
Kseniya <philkins2014@mail.ru>
2023-03-29 20:39:07 +0200

    added task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
+"use strict";
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(users => users.gender.includes(gender)).map(users => users.age).reduce((acc, i, idx, arr) => {
+    acc += i;
+    if (idx === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    } ,0)
+  return users === 0 ? 0 : result;
 }
