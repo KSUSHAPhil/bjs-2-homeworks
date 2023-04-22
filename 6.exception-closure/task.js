146853603b1110c224fa2037eed67d4efba0d56c
Kseniya <philkins2014@mail.ru>
2023-04-22 18:22:14 +0300

    added task 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿function parseCount(value) {
+    let result = Number.parseFloat(value);
+    if (isNaN(result) === true) {
+        throw new Error("Невалидное значение")
+    } else {
+        return result;
+    }
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+
+class Triangle {
+    constructor(a, b, c) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.triangleCheck()
+    }
+
+    triangleCheck() {
+        if ((this.a + this.b) < this.c || (this.b + this.c) < this.a || (this.a + this.c) < this.b) {
+            throw new Error ("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    getPerimeter() {
+       return this.a + this.b + this.c;
+    }
+
+    getArea(){
+        let p = 0.5 * (this.a + this.b + this.c);
+        let s = parseFloat(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+        return s;
+     }
+}
+
+function getTriangle (a, b, c) {
+    try {
+        let t = new Triangle(a, b, c);
+        return t;
+    } catch (err) {
+        return {
+            getArea: function() {return "Ошибка! Треугольник не существует"},
+            getPerimeter: function() {return "Ошибка! Треугольник не существует"}
+          }
+    }
+}
