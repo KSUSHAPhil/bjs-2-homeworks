adae1055fd6a9cbfdd4bb1fcc0f077b6bc7e2096
Kseniya <philkins2014@mail.ru>
2023-04-25 22:03:49 +0300

    added task 8.1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,25 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  return function (...args) {
+    const hash = md5(args);
+    let cacheObject = cache.find((cacheObject) => cacheObject.hash === hash);
+
+    if (cacheObject) {
+        //console.log("Из кэша: " + cacheObject.result);
+        return "Из кэша: " + cacheObject.result;
+    }
+
+    let result = func(...args);
+    cache.push({hash, result});
+
+    if (cache.length > 5) {
+        cache.shift();
+    }
+
+    return "Вычисляем: " + result;
+  }
 }
 
 //Задача № 2
